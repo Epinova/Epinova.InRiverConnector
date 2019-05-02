e5f76e1189e4945bfb6a4220d9323bbe5bdf4278
kjetil.kristiansen <kjetil.kristiansen@epinova.no>
2019-05-02 09:07:24 +0200

    simple restapi to return variations from channelnode

---
+++ EpiserverRestApi/ChannelDataController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Http;
+using inRiver.Remoting;
+using inRiver.Remoting.Objects;
+using inRiver.Remoting.Security;
+
+namespace EpiserverRestApi
+{
+    [RoutePrefix("api/channeldata")]
+    public class ChannelDataController : ApiController
+    {
+        [HttpGet]
+        [Route("GetVariations/{nodeName}")]
+        public IEnumerable<string> GetVariations(string nodeName)
+        {
+            authenticate();
+
+            int channelId = int.Parse(ConfigurationManager.AppSettings["channelId"]);
+            Entity node = RemoteManager.ChannelService.GetEntitiesForChannelAndEntityType(channelId, "ChannelNode")
+                .FirstOrDefault(x => x.DisplayName.Data.ToString().Equals(nodeName, StringComparison.InvariantCultureIgnoreCase));
+            if (node == null)
+                return Enumerable.Empty<string>();
+
+            return RemoteManager.ChannelService.GetAllChannelStructureEntitiesForTypeFromPath($"{channelId}/{node.Id}", "Item")
+                .Select(x => x.Name);
+        }
+
+        private void authenticate()
+        {
+            AuthenticationTicket ticket = RemoteManager.Authenticate(ConfigurationManager.AppSettings["authenticationUrl"], ConfigurationManager.AppSettings["username"], ConfigurationManager.AppSettings["password"]);
+
+            // Initialize RemoteManager 
+            RemoteManager.CreateInstance(ConfigurationManager.AppSettings["authenticationUrl"], ticket);
+        }
+    }
+}
